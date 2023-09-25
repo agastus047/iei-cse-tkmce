6e978d7286bfe94060e9fd59504a7ddfff73218f
Agastus Joyson <agastusjoyson@gmail.com>
2023-09-25 21:16:55 +0530

    set the base repository structure

---
+++ src/pages/test/index.js
@@ -0,0 +1,12 @@
+import TestComponent from "@/components/TestComponent";
+
+export default function TestPage() {
+    return(
+        <div>
+            <div>Test Page</div>
+            <TestComponent/>
+            <TestComponent/>
+            <TestComponent/>
+        </div>
+    );
+}
