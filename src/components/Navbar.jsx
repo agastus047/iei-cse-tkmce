501449be7a0d7f5da9a37bba2100ae39c7060d32
Bharath Vishnu B <bharathvb1204@gmail.com>
2023-09-28 22:49:55 +0530

    navbar_setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
-export default function Navbar() {
-    return(
-        <div>
-            Navbar
-        </div>
-    );
-}
+import Image from "next/image";
+
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-nav-bg flex h-20 xl:h-28 items-center px-6 xl:px-14">
+    </nav>
+  );
+};
+
+export default Navbar;
