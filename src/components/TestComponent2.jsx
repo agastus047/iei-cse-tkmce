b5a81019910cef1bff6212aaa88ee326a2ba59d1
AMALENDU MANOJ <220229@tkmce.ac.in>
2023-09-30 22:55:09 +0530

    s

--- src/components/TestComponent2.jsx
+++ src/components/TestComponent2.jsx
@@ -23,7 +23,7 @@ const TestComponent2 = () => {
   };
   const customBoxShadowStyle4 = {
     position: 'absolute', 
-    background: '#1F2025',
+    background: '#1f2025',
     
     zIndex: '0',
     clipPath: 'polygon(0% 30%, 30% 0%, 100% 0%, 100% 0%, 100% 0%, 100% 100%, 10% 100%, 0% 80%)',
@@ -32,10 +32,10 @@ const TestComponent2 = () => {
 
   return (
     <div className='flex-col'>
-      <div className='flex justify-between md:gap-10 lg:gap-5 xs:gap-5 xxs:gap-5 items-center mb-10 mr-0'>
+      <div className='flex justify-between md:gap-10 lg:gap-5 gap-5 items-center mb-10 mr-0'>
         <div className='flex-col font-Michroma ml-10 lg:ml-20 w-3/6 text-left'>
-          <h1 className='text-white mb-10 xxs:mb-5 font-semibold lg:text-6xl xs:text-2xl xxs:text-2xl xxxs:text-2xl text-5xl'>Vision</h1>
-          <p className='text-white lg:text-2xl text-l font-normal xs:p-2'>"A pathway to technological realm"<br /> We envision to be one of the vibrant professional society of technologists and applied scientists in the world.</p>
+          <h1 className='text-white sm:mb-10 mb-2 font-semibold lg:text-6xl text-xl'>Vision</h1>
+          <p className='text-white lg:text-2xl text-sm font-normal p-2'>"A pathway to technological realm"<br /> We envision to be one of the vibrant professional society of technologists and applied scientists in the world.</p>
         </div>
         <div style={customBoxShadowStyle1}>
           <Image
@@ -44,10 +44,10 @@ const TestComponent2 = () => {
             height={400}
             width={450}
             alt="Picture of the author"
-            className='lg:w-500 lg:h-500 sm:w-72 sm:h-72  xxs:w-40 xxs:h-40 xxxs:w-36 w-64 h-64'
+            className='xl:w-500 xl:h-500 lg:w-400 lg:h-400 sm:w-72 sm:h-72  w-36 h-36 '
           />
         </div>
-        <div style={customBoxShadowStyle4} className='xl:w-350 xl:h-350  xl:right-48 lg:w-350 lg:h-350  lg:right-40  lg:mb-36 md:right-16 md:w-64 md:h-64  sm:right-16 sm:w-60 sm:h-60 min-[390px]:right-12 max-[640px]:right-12     xxs:right-8 xxs:w-32 xxs:h-32  xxxs:w-12 xxxs:right-3 w-48 h-52  mb-8 '></div>
+        <div style={customBoxShadowStyle4} className='xl:w-350 xl:h-350  xl:right-48 lg:w-300 lg:h-300  lg:right-32  lg:mb-32 md:right-16 md:w-64 md:h-64  sm:right-16 sm:w-60 sm:h-60 min-[390px]:right-12 max-[640px]:right-12        w-28 h-28 mb-8' ></div>
       </div>
 
       <div className='flex justify-between lg:gap-20 items-center'>
@@ -59,15 +59,15 @@ const TestComponent2 = () => {
         width={500}
         height={500}
         alt="Picture of the author"
-        className='lg:w-500 lg:h-500 sm:w-72 sm:h-72  xxs:w-40 xxs:h-40 w-44 h-64'
+        className='xl:w-500 xl:h-500 lg:w-400 lg:h-400 sm:w-72 sm:h-72  w-36 h-36 '
       />
     </div>
-    <div style={customBoxShadowStyle3} className='lg:w-400 lg:h-400 lg:ml-20 lg:mb-64 sm:w-64 sm:h-64   sm:mb-12  xxs:w-32 xxs:h-32 xxs:mb-48 xxs:ml-6 w-52 h-64  ml-2'>
+    <div style={customBoxShadowStyle3} className='xl:w-400 xl:h-400 lg:w-350 lg:h-350  lg:ml-10 lg:mb-64 sm:w-64 sm:h-64   sm:mb-12  w-28 h-28 ml-7 mb-48'>
       </div>
 
-      <div className='flex-col font-Michroma mr-5 lg:mr-10 w-3/6 text-right'>
-        <h1 className='text-white mb-10 font-semibold lg:text-6xl xs:text-2xl xxs:text-2xl  text-5xl'>Mission</h1>
-        <p className='text-white lg:text-2xl text-l font-normal'>Our mission at IEI CSE SB TKMCE is to empower computer science enthusiasts like you with an unparalleled experience. We're not just a club; we're a dynamic hub for future computer science engineers!</p>
+      <div className='flex-col font-Michroma mr-5 lg:mr-10 w-3/6 text-right items-center'>
+        <h1 className='text-white sm:mb-10 font-semibold lg:text-6xl text-xl mb-2  '>Mission</h1>
+        <p className='text-white lg:text-2xl text-sm font-normal'>Our mission at IEI CSE SB TKMCE is to empower computer science enthusiasts like you with an unparalleled experience. We're not just a club; we're a dynamic hub for future computer science engineers!</p>
       </div>
     </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,28 +11,22 @@ module.exports = {
         '500': '500px',
         '400':'400px',
         '350':'350px',
+        '350':'300px',
+
       },
       width: {
         '500': '500px',
         '400':'400px',
         '450':'450px',
         '350':'350px',
+        '300':'300px',
 
        },
       fontFamily:{
         Michroma:['Michroma'],
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      screens:{
-        'xs':'390px',
-        
-        'xxs':'360px',
-        'xxxs':'300px'
-      }
+     
+      
     },
   },
   plugins: [],
