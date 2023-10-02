15ad21ac4360dea0aa98c94934ebcab904075b2e
AMALENDU MANOJ <220229@tkmce.ac.in>
2023-10-02 11:12:23 +0530

    inner border left

--- src/components/TestComponent2.jsx
+++ src/components/TestComponent2.jsx
@@ -29,7 +29,7 @@ const TestComponent2 = () => {
     clipPath: 'polygon(0% 30%, 30% 0%, 100% 0%, 100% 0%, 100% 0%, 100% 100%, 10% 100%, 0% 80%)',
    
   };
-
+  
   return (
     <div className='flex-col'>
       <div className='flex justify-between md:gap-10 lg:gap-5 gap-5 items-center mb-10 mr-0'>
@@ -37,21 +37,22 @@ const TestComponent2 = () => {
           <h1 className='text-white sm:mb-10 mb-2 font-semibold lg:text-6xl text-xl'>Vision</h1>
           <p className='text-white lg:text-2xl text-sm font-normal p-2'>"A pathway to technological realm"<br /> We envision to be one of the vibrant professional society of technologists and applied scientists in the world.</p>
         </div>
+        
         <div style={customBoxShadowStyle1}>
-          <Image
+          <img src="/vision.png"
             
-            src="/vision.png"
-            height={400}
-            width={450}
             alt="Picture of the author"
-            className='xl:w-500 xl:h-500 lg:w-400 lg:h-400 sm:w-72 sm:h-72  w-36 h-36 '
-          />
+            className='xl:w-500 xl:h-500 lg:w-400 lg:h-400 sm:w-72 sm:h-72  w-36 h-36   '
+          ></img>
+          
         </div>
+        
         <div style={customBoxShadowStyle4} className='xl:w-350 xl:h-350  xl:right-48 lg:w-300 lg:h-300  lg:right-32  lg:mb-32 md:right-16 md:w-64 md:h-64  sm:right-16 sm:w-60 sm:h-60 min-[390px]:right-12 max-[640px]:right-12        w-28 h-28 mb-8' ></div>
-      </div>
+        
+        </div>
 
       <div className='flex justify-between lg:gap-20 items-center'>
-      <div className='lg:w-500 lg:h-500  w-3/6   h-64'>
+      <div className='lg:w-500 lg:h-500  w-3/6   h-64 '>
       <Image
 
         style={customBoxShadowStyle2}
@@ -59,7 +60,7 @@ const TestComponent2 = () => {
         width={500}
         height={500}
         alt="Picture of the author"
-        className='xl:w-500 xl:h-500 lg:w-400 lg:h-400 sm:w-72 sm:h-72  w-36 h-36 '
+        className='xl:w-500 xl:h-500 lg:w-400 lg:h-400 sm:w-72 sm:h-72  w-36 h-36  '
       />
     </div>
     <div style={customBoxShadowStyle3} className='xl:w-400 xl:h-400 lg:w-350 lg:h-350  lg:ml-10 lg:mb-64 sm:w-64 sm:h-64   sm:mb-12  w-28 h-28 ml-7 mb-48'>
