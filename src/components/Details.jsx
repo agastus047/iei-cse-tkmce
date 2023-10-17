eb23b2edd27c9e1908596d068e7d5cf1feac5f7d
Bharath Vishnu B <bharathvb1204@gmail.com>
2023-10-18 01:32:16 +0530

    changed the details bar

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,30 +2,32 @@ import React from 'react';
 
 function Details() {
   return (
-    <div className="bg-white h-auto">
-      <div className="bg-white py-3 md:py-0 md:grid md:grid-cols-3 gap-10">
-        <div className="ml-20 mr-20 mt-5 md:mr-0 md:ml-40 md:mt-20 md:mb-20 relative bg-white h-auto p-4 text-center">
-          <div className="absolute top-0 left-0 w-8 h-8 bg-white border-t-4 border-l-4 border-black rounded-lg"></div>
-          <div className="absolute top-0 right-0 w-8 h-8 bg-white border-t-4 border-r-4 border-black rounded-lg"></div>
-          <div className="absolute bottom-0 left-0 w-8 h-8 bg-white border-b-4 border-l-4 border-black rounded-lg"></div>
-          <div className="absolute bottom-0 right-0 w-8 h-8 bg-white border-b-4 border-r-4 border-black rounded-lg"></div>
-          <h1 className='text-xl mt-4 md:text-5xl font-Poppins font-bold'>Established</h1>
+    <div className="bg-white p-4 md:p-8">
+      <div className="grid mt-10 mb-10 grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="relative bg-white p-4 md:p-6 text-center rounded-lg">
+          <div className="absolute top-0 left-0 w-12 h-12 bg-white border-t-4 border-l-4 border-black rounded-lg"></div>
+          <div className="absolute top-0 right-0 w-12 h-12 bg-white border-t-4 border-r-4 border-black rounded-lg"></div>
+          <div className="absolute bottom-0 left-0 w-12 h-12 bg-white border-b-4 border-l-4 border-black rounded-lg"></div>
+          <div className="absolute bottom-0 right-0 w-12 h-12 bg-white border-b-4 border-r-4 border-black rounded-lg"></div>
+          <h1 className='text-xl md:text-5xl font-Poppins font-bold mt-4'>Established</h1>
           <h2 className='text-2xl md:text-8xl font-Poppins font-bold'>1935</h2>
         </div>
-        <div className="ml-20 mr-20 mt-5 md:ml-20 md:mr-10 md:mt-20 md:mb-20 h-auto relative bg-white p-4 text-center">
-          <div className="absolute top-0 left-0 w-8 h-8 bg-white border-t-4 border-l-4 border-black rounded-lg"></div>
-          <div className="absolute top-0 right-0 w-8 h-8 bg-white border-t-4 border-r-4 border-black rounded-lg"></div>
-          <div className="absolute bottom-0 left-0 w-8 h-8 bg-white border-b-4 border-l-4 border-black rounded-lg"></div>
-          <div className="absolute bottom-0 right-0 w-8 h-8 bg-white border-b-4 border-r-4 border-black rounded-lg"></div>
-          <h1 className='text-xl mt-4 md:text-5xl font-Poppins font-bold'>Members//</h1>
+
+        <div className="relative bg-white p-4 md:p-6 text-center mt-4 md:mt-0 rounded-lg">
+          <div className="absolute top-0 left-0 w-12 h-12 bg-white border-t-4 border-l-4 border-black rounded-lg"></div>
+          <div className="absolute top-0 right-0 w-12 h-12 bg-white border-t-4 border-r-4 border-black rounded-lg"></div>
+          <div className="absolute bottom-0 left-0 w-12 h-12 bg-white border-b-4 border-l-4 border-black rounded-lg"></div>
+          <div className="absolute bottom-0 right-0 w-12 h-12 bg-white border-b-4 border-r-4 border-black rounded-lg"></div>
+          <h1 className='text-xl md:text-5xl font-Poppins font-bold mt-4'>Members</h1>
           <h2 className='text-2xl md:text-8xl font-Poppins font-bold'>223+</h2>
         </div>
-        <div className="ml-20 mr-20 mt-5 mb-5 md:ml-10 md:mr-40 md:mt-20 md:mb-20  h-auto relative bg-white p-4 text-center">
-          <div className="absolute top-0 left-0 w-8 h-8 bg-white border-t-4 border-l-4 border-black rounded-lg"></div>
-          <div className="absolute top-0 right-0 w-8 h-8 bg-white border-t-4 border-r-4 border-black rounded-lg"></div>
-          <div className="absolute bottom-0 left-0 w-8 h-8 bg-white border-b-4 border-l-4 border-black rounded-lg"></div>
-          <div className="absolute bottom-0 right-0 w-8 h-8 bg-white border-b-4 border-r-4 border-black rounded-lg"></div>
-          <h1 className='text-xl mt-4 md:text-5xl font-Poppins font-bold'>Events//</h1>
+
+        <div className="relative bg-white p-4 md:p-6 text-center mt-4 md:mt-0 rounded-lg">
+          <div className="absolute top-0 left-0 w-12 h-12 bg-white border-t-4 border-l-4 border-black rounded-lg"></div>
+          <div className="absolute top-0 right-0 w-12 h-12 bg-white border-t-4 border-r-4 border-black rounded-lg"></div>
+          <div className="absolute bottom-0 left-0 w-12 h-12 bg-white border-b-4 border-l-4 border-black rounded-lg"></div>
+          <div className="absolute bottom-0 right-0 w-12 h-12 bg-white border-b-4 border-r-4 border-black rounded-lg"></div>
+          <h1 className='text-xl md:text-5xl font-Poppins font-bold mt-4'>Events</h1>
           <h2 className='text-2xl md:text-8xl font-Poppins font-bold'>007+</h2>
         </div>
       </div>
