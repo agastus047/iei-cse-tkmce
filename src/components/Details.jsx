7e76c9b93448f147756387a5db393fb56d0d53d9
Bharath Vishnu B <bharathvb1204@gmail.com>
2023-10-18 01:48:11 +0530

    resposnive details card

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -4,16 +4,16 @@ function Details() {
   return (
     <div className="bg-white p-4 md:p-8">
       <div className="grid mt-10 mb-10 grid-cols-1 md:grid-cols-3 gap-4">
-        <div className="ml-40 mr-40 md:ml-10 md:mr-10 relative bg-white p-4 md:p-6 text-center rounded-lg">
+        <div className="ml-20 mr-20 md:ml-10 md:mr-10 relative bg-white p-4 md:p-6 text-center rounded-lg">
           <div className="absolute top-0 left-0 w-4 h-4 md:w-12 md:h-12 bg-white border-t-4 border-l-4 border-black rounded-lg"></div>
           <div className="absolute top-0 right-0 w-4 h-4 md:w-12 md:h-12 bg-white border-t-4 border-r-4 border-black rounded-lg"></div>
           <div className="absolute bottom-0 left-0 w-4 h-4 md:w-12 md:h-12 bg-white border-b-4 border-l-4 border-black rounded-lg"></div>
           <div className="absolute bottom-0 right-0 w-4 h-4 md:w-12 md:h-12 bg-white border-b-4 border-r-4 border-black rounded-lg"></div>
-          <h1 className='text-xl md:text-5xl font-Poppins font-bold mt-4'>Established</h1>
+          <h1 className='text-xl md:text-5xl font-Poppins font-bold mt-4 mx-0'>Established</h1>
           <h2 className='text-2xl md:text-8xl font-Poppins font-bold'>1935</h2>
         </div>
 
-        <div className="ml-40 mr-40 md:ml-10 md:mr-10 relative bg-white p-4 md:p-6 text-center mt-4 md:mt-0 rounded-lg">
+        <div className="ml-20 mr-20 md:ml-10 md:mr-10 relative bg-white p-4 md:p-6 text-center mt-4 md:mt-0 rounded-lg">
           <div className="absolute top-0 left-0 w-4 h-4 md:w-12 md:h-12 bg-white border-t-4 border-l-4 border-black rounded-lg"></div>
           <div className="absolute top-0 right-0 w-4 h-4 md:w-12 md:h-12 bg-white border-t-4 border-r-4 border-black rounded-lg"></div>
           <div className="absolute bottom-0 left-0 w-4 h-4 md:w-12 md:h-12 bg-white border-b-4 border-l-4 border-black rounded-lg"></div>
@@ -22,7 +22,7 @@ function Details() {
           <h2 className='text-2xl md:text-8xl font-Poppins font-bold'>223+</h2>
         </div>
 
-        <div className="ml-40 mr-40 md:ml-10 md:mr-10 relative bg-white p-4 md:p-6 text-center mt-4 md:mt-0 rounded-lg">
+        <div className="ml-20 mr-20 md:ml-10 md:mr-10 relative bg-white p-4 md:p-6 text-center mt-4 md:mt-0 rounded-lg">
           <div className="absolute top-0 left-0 w-4 h-4 md:w-12 md:h-12 bg-white border-t-4 border-l-4 border-black rounded-lg"></div>
           <div className="absolute top-0 right-0 w-4 h-4 md:w-12 md:h-12 bg-white border-t-4 border-r-4 border-black rounded-lg"></div>
           <div className="absolute bottom-0 left-0 w-4 h-4 md:w-12 md:h-12 bg-white border-b-4 border-l-4 border-black rounded-lg"></div>
