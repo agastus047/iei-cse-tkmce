17226c8bf8b3b6b8b2c7b0ee4a1b2e946ba0d7e8
Bharath Vishnu B <bharathvb1204@gmail.com>
2023-11-03 00:37:00 +0530

    responsiveness

--- src/components/ScrollTrigger2.jsx
+++ src/components/ScrollTrigger2.jsx
@@ -4,12 +4,12 @@ function ScrollTrigger2() {
   return (
     <div className="bg-hero-bg p-4 md:p-8">
       <div className="flex md:flex-row justify-between md:gap-24 mt-20">
-        <div className="font-Poppins font-bold text-xl md:text-4xl text-black md:text-right">
+        <div className="font-Poppins font-bold md:text-6xl sm:text-4xl text-xl text-black md:text-right">
           WHAT WE DO?
         </div>
         <div className="h-1 w-40 mt-10 md:mt-5 md:w-96 md:text-left bg-cyanblue"></div>
       </div>
-      <div className="text-black text-md md:text-2xl font-Poppins mt-4 md:mt-6 md:text-right mb-20">
+      <div className="text-black md:text-2xl text-sm font-Poppins mt-4 md:mt-6 md:text-right mb-20">
         <p>Exclusive Resources: Dive into a treasure trove of cutting-edge
         resources, from the latest tech trends to in-depth coding tutorials – we've got your back.</p>
         <p>
