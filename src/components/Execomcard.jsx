c34243f202ea1ee4e4b374753ed964e50fe637d2
AMALENDU MANOJ <220229@tkmce.ac.in>
2023-11-18 18:59:54 +0530

    execom page

---
+++ src/components/Execomcard.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+const Execomcard = ({ items }) => {
+  const iconStyle = {
+    fontSize: '90px', // Default size
+    '@media (max-width: 768px)': {
+      fontSize: '60px', // Adjust size for screens smaller than 768px
+    },
+    '@media (min-width: 1440px)': {
+      fontSize: '100px', // Adjust size for screens smaller than 768px
+    },
+    // Add more media queries and size adjustments as needed
+  };
+
+  const handleLinkedInClick = () => {
+    window.open("https://www.linkedin.com/in/your-profile", '_blank');
+  };
+
+  return (
+    <div className='group [perspective:1000px] mt-10 '>
+      <div className='w-60 h-72 lg:w-60 lg:h-72 m-auto bg-white flex flex-col border-4 border-cyan-500 p-2 transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] -z-50'>
+      <div className="card-image w-full h-60 lg:h-60 relative overflow-hidden inset-0">
+      {items.imgcorrect ? (
+        <img
+          src={items.img}
+          className="absolute object-cover h-full w-full"
+          alt="Image"
+          style={{ zIndex: 0, pointerEvents: 'none' }}
+        />
+      ) : (
+        <img
+          src={items.img}
+          className="absolute object-center h-full w-full"
+          alt="Image"
+          style={{ zIndex: 0, pointerEvents: 'none' }}
+        />
+      )}
+    </div>
+        <div className='flex flex-col justify-start items-start text-3xl text-black font-bold font-Poppins'>
+          <p className='text-xl'>{items.name}</p>
+          <p className='text-xs'>{items.position}</p>
+        </div>
+        <div className='absolute inset-0 h-full w-full text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden] [transform-style:preserve-3d] z-50' style={{ backgroundColor: "#00ADB5" }}>
+          <div className='flex justify-center items-center h-full z-50'>
+            <LinkedInIcon sx={{ ...iconStyle, zIndex: 5, cursor: 'pointer' }} onClick={handleLinkedInClick} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Execomcard;
---
+++ src/pages/Execom.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import execomlist from '@/components/execomlist'
+import Execomcard from '@/components/Execomcard'
+const Execom = () => {
+  return (
+    <div className='flex-col  justify-items-center '>
+        <div  className='flex flex-col justify-end items-center h-32 w-3/6'>
+            <h1 className="font-Poppins font-extrabold text-4xl text-center" style={{color:"#00ADB5"}}>TEAM</h1>
+           
+        </div>
+        <hr style={{top: '20px',border: 'none',height: '4px',background: '#393E46',marginBottom:'10px',width:'50%',float:'left'}}></hr>
+        <span style={{ backgroundColor: '#00ADB5', height: '60px', width: '100%', display: 'block' }} className='mt-10'>
+        <div className='flex justify-center pt-3 items-center'><p className='text-white font-extrabold text-3xl'>EXECOM</p></div>
+        </span>
+        <div className='xl:pl-10 xl:pr-10  '>
+        <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 mb-10 grid-cols-1'>
+        {
+            execomlist.map(item => (
+                
+                  <Execomcard items={item}/>      
+        ))
+        }
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Execom
