4e3a6dcad21864b36924585a5f4283e0912c1199
AMALENDU MANOJ <220229@tkmce.ac.in>
2023-11-29 23:19:47 +0530

    event page

--- src/components/eventlist.js
+++ src/components/eventlist.js
@@ -2,24 +2,28 @@ const eventlist=[
     {
         id:0,
         name:'Event1',
+        discription:'event1',
         img:'/assets/event.png',
         link:'event1'
     },
     {
         id:1,
         name:'Event2',
+        discription:'event1',
         img:'/assets/event.png',
         link:'event2'
     },
     {
         id:2,
         name:'Event3',
+        discription:'event1',
         img:'/assets/event.png',
         link:'event3'
     },
     {
         id:3,
         name:'Event4',
+        discription:'event1',
         img:'/assets/event.png',
         link:'event4'
     },
--- src/pages/even/[name].jsx
+++ src/pages/even/[name].jsx
@@ -3,9 +3,61 @@ import React from 'react'
 
 const Eventpage = ({eventdata}) => {
   return (
-    <div className='col'>
-      <div className='col'>
-      {eventdata.name}
+    <div className=' xl:pr-72 xl:pl-72 lg:pr-64 lg:pl-64 md:pr-40 sm:pr-20 sm:pl-20 pl-2 pr-2 md:pl-40 pb-10 pt-10 '>
+      <div className='border-4 flex flex-col gap-10 bg-white shadow-lg md:p-20 p-5'>
+        <img src={eventdata.img} className="z-10 w-96 h-96" alt="Image"/>
+        <h1 className='text-3xl'>{eventdata.name}</h1>
+        <p>{eventdata.discription}</p>
+        <div className='flex flex-col'>
+          <h3 className='font-bold'>Price</h3>
+          <p>Rs.100</p>
+        </div>
+        <div className='flex justify-between'>
+          
+          <div className='flex flex-col'>
+            <h3 className='font-bold'>Start Date</h3>
+            <p>06-10-2023</p>
+          </div>
+          <div className='flex flex-col'>
+            <h3 className='font-bold'>End Date</h3>
+            <p>10-10-2023</p>
+          </div>
+          <div className='flex flex-col'>
+            <h3 className='font-bold'>Venue</h3>
+            <p>Tkm</p>
+          </div>
+        </div>
+        <div>
+          <p className='underline 4xl'>REGISTRATION DETAILS</p>
+        </div>
+        <div className='flex justify-between'>
+          
+          <div className='flex flex-col'>
+            <h3 className='font-bold'>Start Date</h3>
+            <p>06-10-2023</p>
+          </div>
+          <div className='flex flex-col'>
+            <h3 className='font-bold'>End Date</h3>
+            <p>10-10-2023</p>
+          </div>
+          <div className='flex flex-col'>
+            <h3 className='font-bold'>Fees</h3>
+            <p>Rs.1000</p>
+          </div>
+        </div>
+        <div>
+          <p className='underline 4xl'>COORDINATORS</p>
+        </div>
+        <div className='flex justify-between'>
+          <div className='flex flex-col'>
+            <h3 className='font-bold'>Amalendu</h3>
+            <p>+8078213133</p>
+          </div>
+          <div className='flex flex-col'>
+            <h3 className='font-bold'>Amalendu</h3>
+            <p>+8078213133</p>
+          </div>
+        </div>
       </div>
     </div>
   )
