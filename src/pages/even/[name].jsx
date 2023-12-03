4cbd43f80da65fda222a8e4b0a45f26845a500fd
AMALENDU MANOJ <220229@tkmce.ac.in>
2023-12-03 16:29:05 +0530

    event page

--- src/pages/even/[name].jsx
+++ src/pages/even/[name].jsx
@@ -3,66 +3,55 @@ import React from 'react'
 
 const Eventpage = ({eventdata}) => {
   return (
-    <div className=' xl:pr-72 xl:pl-72 lg:pr-64 lg:pl-64 md:pr-40 sm:pr-20 sm:pl-20 pl-2 pr-2 md:pl-40 pb-10 pt-10 '>
-      <div className='border-4 flex flex-col gap-10 bg-white shadow-lg md:p-20 p-5'>
-        <img src={eventdata.img} className="z-10 w-96 h-96" alt="Image"/>
-        <h1 className='text-3xl'>{eventdata.name}</h1>
-        <p>{eventdata.discription}</p>
-        <div className='flex flex-col'>
-          <h3 className='font-bold'>Price</h3>
-          <p>Rs.100</p>
+    <div className=' xl:pr-64 xl:pl-64 lg:pr-36 lg:pl-36 md:pr-10 sm:pr-20 sm:pl-20 pl-2 pr-2 md:pl-10 pb-10 pt-10 '>
+      <div className='border-4  md:flex  md:gap-10 gap-10 bg-white shadow-lg md:p-5 p-5'>
+        <div className='md:w-1/2 md:block flex justify-center  '>
+          <img src={eventdata.img} className="z-10 w-96 h-96" alt="Image" />
         </div>
-        <div className='flex justify-between'>
+        <div className='flex flex-col gap-4 md:w-1/2 '>
+          <h1 className='text-3xl text-center'>{eventdata.name}</h1>
+          <p>{eventdata.discription}</p>
+          <div className='flex flex-col '>
+            <h3 className='font-bold'>Price</h3>
+            <p>Rs.100</p>
+          </div>
+          <div className='flex justify-between gap-10'>
+
+            <div className='flex flex-col'>
+              <h3 className='font-bold'>Start Date</h3>
+              <p>06-10-2023</p>
+            </div>
+            <div className='flex flex-col'>
+              <h3 className='font-bold'>End Date</h3>
+              <p>10-10-2023</p>
+            </div>
+            <div className='flex flex-col'>
+              <h3 className='font-bold'>Venue</h3>
+              <p>Tkm</p>
+            </div>
+          </div>
+         
           
-          <div className='flex flex-col'>
-            <h3 className='font-bold'>Start Date</h3>
-            <p>06-10-2023</p>
+          <div>
+            <p className='underline 4xl font-bold'>COORDINATORS</p>
           </div>
-          <div className='flex flex-col'>
-            <h3 className='font-bold'>End Date</h3>
-            <p>10-10-2023</p>
+          <div className='flex justify-between'>
+            <div className='flex flex-col'>
+              <h3 className='font-bold'>Amalendu</h3>
+              <p>+8078213133</p>
+            </div>
+            <div className='flex flex-col'>
+              <h3 className='font-bold'>Amalendu</h3>
+              <p>+8078213133</p>
+            </div>
           </div>
-          <div className='flex flex-col'>
-            <h3 className='font-bold'>Venue</h3>
-            <p>Tkm</p>
-          </div>
-        </div>
-        <div>
-          <p className='underline 4xl'>REGISTRATION DETAILS</p>
-        </div>
-        <div className='flex justify-between'>
-          
-          <div className='flex flex-col'>
-            <h3 className='font-bold'>Start Date</h3>
-            <p>06-10-2023</p>
-          </div>
-          <div className='flex flex-col'>
-            <h3 className='font-bold'>End Date</h3>
-            <p>10-10-2023</p>
-          </div>
-          <div className='flex flex-col'>
-            <h3 className='font-bold'>Fees</h3>
-            <p>Rs.1000</p>
+          <div className='flex justify-end'>
+            <button class="bg-black hover:bg-white text-white hover:text-black font-bold p-4 w-40 ">
+              Register
+            </button>
           </div>
         </div>
-        <div>
-          <p className='underline 4xl'>COORDINATORS</p>
-        </div>
-        <div className='flex justify-between'>
-          <div className='flex flex-col'>
-            <h3 className='font-bold'>Amalendu</h3>
-            <p>+8078213133</p>
-          </div>
-          <div className='flex flex-col'>
-            <h3 className='font-bold'>Amalendu</h3>
-            <p>+8078213133</p>
-          </div>
-        </div>
-        <div>
-          <button class="bg-black hover:bg-white text-white hover:text-black font-bold p-4 w-40 ">
-            Register
-          </button>
-        </div>
+
       </div>
     </div>
   )
