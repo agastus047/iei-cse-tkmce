9c90900f505bd3f2d2edcba90f54d98a96eebb56
Agastus Joyson <agastusjoyson@gmail.com>
2023-12-18 23:03:05 +0530

    change to link

--- src/components/Eventspgcard.jsx
+++ src/components/Eventspgcard.jsx
@@ -1,9 +1,10 @@
-import Router from "next/router";
+import Link from "next/link";
 
 const Eventcard =({items})=>
 {
     return(
-        <div className="md:mt-5 w-64 md:w-72 bg-white lg:w-84 h-84 m-auto flex flex-col rounded-lg border-4 border-cyan-500  transform transition-transform hover:scale-105 " onClick={() => Router.push("/event/" + items.link)}>
+        <Link href={`/event/${items.link}`}>
+        <div className="md:mt-5 w-64 md:w-72 bg-white lg:w-84 h-84 m-auto flex flex-col rounded-lg border-4 border-cyan-500  transform transition-transform hover:scale-105 ">
         <div className="mt-2 mr-2 ml-2 bg-white card-image w-auto h-72 relative overflow-hidden rounded-lg shadow-lg">
             <img src={items.img} className="absolute bg-white object-cover object-center h-full w-full rounded-t-md z-10" alt="Image"/>
         </div>
@@ -11,6 +12,7 @@ const Eventcard =({items})=>
           {items.name} 
         </div>
     </div>
+    </Link>
     )
 }
 
