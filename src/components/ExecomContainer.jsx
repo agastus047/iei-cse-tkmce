1719b82aad96d4004ee0bd14bc3bb2d02c952efd
Bharath Vishnu B <bharathvb1204@gmail.com>
2024-02-18 23:46:04 +0530

    execom container update2

--- src/components/ExecomContainer.jsx
+++ src/components/ExecomContainer.jsx
@@ -4,24 +4,26 @@ import Link from 'next/link';
 
 const ExecutiveTeam = () => {
   return (
-    <div className="flex flex-col items-center justify-center bg-white">
-      <div className="grid grid-cols-1 sm:grid sm:grid-cols-3 lg:grid-cols-3 gap-4 md:gap-8">
+    <div className="flex flex-col items-center justify-center bg-hero-bg">
+      <div className="text-5xl mx-auto mt-10  font-bold text-[#393E46]">EXECOM</div>
+      <div className="grid grid-cols-1 sm:grid sm:grid-cols-3 lg:grid-cols-3 gap-1 md:gap-8">
+        
         <div className="mt-0 md:mt-10 rounded-lg overflow-hidden p-6 mx-auto">
           
           <img src='/assets/execommember/Sanjay S J.jpg' className="border-4 border-cyanblue shadow-md  w-48 h-48 md:w-64 md:h-64 object-cover rounded-lg" />
-          <h2 className="text-xl font-semibold mb-4 text-center">CHAIRPERSON</h2>
+          <h2 className="text-xl font-semibold mb-4 text-center">Chairperson</h2>
 
         </div>
         <div className="rounded-lg overflow-hidden p-6 mx-auto">
          
-          <img src='/assets/execommember/Sanjay S J.jpg' className="ml-5 md:ml-0 border-4 border-cyablue shadow-md w-48 h-48 md:w-64 md:h-64 object-cover rounded-lg" />
-          <h2 className="text-xl font-semibold mb-4 text-center">FACULTY COORDINATOR</h2>
+          <img src='/assets/execommember/Sanjay S J.jpg' className="border-4 border-cyanblue shadow-md w-48 h-48 md:w-64 md:h-64 object-cover rounded-lg" />
+          <h2 className="text-xl font-semibold mb-4 text-center ml-4 md:ml-0">Faculty Coordinator</h2>
 
         </div>
         <div className="mt-0 md:mt-10 rounded-lg overflow-hidden p-6 mx-auto">
 
-          <img src='/assets/execommember/Mohammed sinan pakkada .jpg' className="ml-5 border-4 border-cyablue shadow-md  w-48 h-48 md:w-64 md:h-64 object-cover rounded-lg" />
-          <h2 className="text-xl font-semibold mb-4 text-center">GENERAL SECRETARY</h2>
+          <img src='/assets/execommember/Mohammed sinan pakkada .jpg' className="border-4 border-cyanblue shadow-md  w-48 h-48 md:w-64 md:h-64 object-cover rounded-lg" />
+          <h2 className="text-xl font-semibold mb-4 text-center ml-4 md:ml-0">General Secretary</h2>
 
         </div>
       </div>
