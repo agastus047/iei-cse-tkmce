134a0c52feeb5a449edb1d9bce2267a7768b2006
AMALENDU MANOJ <220229@tkmce.ac.in>
2024-02-23 23:29:46 +0530

    changed

--- src/pages/event/[name].jsx
+++ src/pages/event/[name].jsx
@@ -5,19 +5,23 @@ import { useRouter } from 'next/router';
 const Eventpage = () => {
 
   const router = useRouter();
-  const {name} = router.query;
-  
+  const { name } = router.query;
+
   const eventdata = eventlist.find((event) => event.link === name);
-  
+
   return (
     <div className=' xl:pr-64 xl:pl-64 lg:pr-36 lg:pl-36 md:pr-10 sm:pr-20 sm:pl-20 pl-2 pr-2 md:pl-10 pb-10 pt-10 '>
-      <div className='border-4  md:flex  md:gap-10 gap-10 bg-white shadow-lg md:p-5 p-5 font-Poppins'>
-        <div className='md:w-1/2 md:block flex justify-center  '>
-          <img src={eventdata?.img} className="z-10 sm:w-96 sm:h-96 w-64 h-64" alt="Image" />
+      <div className='border-4  md:block  md:gap-10 gap-10 bg-white shadow-lg md:p-5 p-5 font-Poppins'>
+        <div className='md:w-full  md:flex block md:justify-between md:items-center md:gap-10 '>
+          <div className='flex justify-center md:w-1/2 '>
+          <img src={eventdata?.img} className="z-10 sm:w-96 sm:h-96 w-64 h-64 mb-10 md:mb-0" alt="Image" />
+          </div>
+          <div className='flex flex-col md:w-1/2 mb-10 '>
+            <h1 className='text-3xl text-center font-Poppins font-bold mt-10 mb-8'>{eventdata?.name}</h1>
+            <p className='font-Poppins'>{eventdata?.discription}</p>
+          </div>
         </div>
-        <div className='flex flex-col gap-4 md:w-1/2 '>
-          <h1 className='text-3xl text-center font-Poppins font-bold'>{eventdata?.name}</h1>
-          <p className='font-Poppins'>{eventdata?.discription}</p>
+        <div className='flex flex-col gap-4 md:w-full '>
           <div className='flex flex-col '>
             <h3 className='font-bold '>Price</h3>
             <p>Rs.100</p>
@@ -37,8 +41,8 @@ const Eventpage = () => {
               <p>Tkm</p>
             </div>
           </div>
-         
-          
+
+
           <div>
             <p className=' 4xl font-bold'>COORDINATORS</p>
           </div>
