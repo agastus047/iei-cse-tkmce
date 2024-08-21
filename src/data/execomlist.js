32dd975d9cbcd7f034e17946a1255e14637803ec
GAUTHAM  BINU <gauthambinu.edu@gmail.com>
2024-08-22 00:20:23 +0530

    execom list updated

--- src/data/execomlist.js
+++ src/data/execomlist.js
@@ -214,6 +214,192 @@ export const webteam23=[
     },
 ]
 export const execomlist24 = [
+    {
+        id:0,
+        name:'Anagha Mohan',
+        img:'/assets/execommember/Anagha Mohan.jpeg',
+        position:'Chairperson',
+        linkedIn:'https://www.linkedin.com/in/anagha-mohan-8a58a5257/',
+        imgcorrect:true
+    },
+    {
+        id:1,
+        name:'K R ABHINAND',
+        img:'/assets/execommember/K R ABHINAND.jpg',
+        position:'Secretary',
+        linkedIn:'https://www.linkedin.com/in/k-r-abhinand-babu-32a1a8259/',
+        imgcorrect:true
+    },
+    {
+        id:2,
+        name:'Archana V M',
+        img:'/assets/execommember/Archana V M.jpg',
+        position:'Treasurer',
+        linkedIn:'https://www.linkedin.com/in/archana-vm-056705260/',
+        imgcorrect:false
+    },
+    {
+        id:3,
+        name:'Joel Paul John',
+        img:'/assets/execommember/Joel Paul.jpg',
+        position:'Program Head',
+        linkedIn:'https://www.linkedin.com/in/joel-paul-john/',
+        imgcorrect:true
+    },
+    {
+        id:3,
+        name:'Vrindha Vinod',
+        img:'/assets/execommember/Vrindha Vinod .jpg',
+        position:'Program Head',
+        linkedIn:'https://www.linkedin.com/in/vrindhavinod/',
+        imgcorrect:true
+    },
+    {
+        id:4,
+        name:'Amalendu Manoj',
+        img:'/assets/execommember/AMALENDU MANOJ.jpg',
+        position:'Tech Head',
+        linkedIn:'https://www.linkedin.com/in/amalendu-manoj-970a13253/',
+        imgcorrect:true
+    },
+    {
+        id:5,
+        name:'Amarnad K M',
+        img:'/assets/execommember/Amarnad.jpg',
+        position:'Design Head',
+        linkedIn:'https://www.linkedin.com/in/amarnad-k-m-3a80541a7/',
+        imgcorrect:true
+    },
+    {
+        id:6,
+        name:'Asiya Muhammed',
+        img:'/assets/execommember/Asiya Muhammed .jpg',
+        position:'Documentation Head',
+        linkedIn:'https://www.linkedin.com/in/asiya-muhammed-095a97258/',
+        imgcorrect:true
+    },
+    
+    {
+        id:7,
+        name:'Ashwin Shankar',
+        img:'/assets/execommember/Ashwin.jpg',
+        position:'Media Head',
+        linkedIn:"https://www.linkedin.com/in/ashwin-shankar-a5a138318/",
+        imgcorrect:true
+    },
+    {
+        id:8,
+        name:'Anu Thomson',
+        img:'/assets/execommember/ANU THOMSON.jpg',
+        position:'Program Coordinator',
+        linkedIn:'https://www.linkedin.com/in/anu-thomson-9b9636291/',
+        imgcorrect:true
+    },
+    {
+        id:9,
+        name:'Ann Maria',
+        img:'/assets/execommember/Ann Maria Jose.jpg',
+        position:'Program Coordinator',
+        linkedIn:'https://www.linkedin.com/in/ann-maria-jose-652068286/',
+        imgcorrect:true
+    },
+    {
+        id:10,
+        name:'Aishwarya Prakash',
+        img:'/assets/execommember/Aishwarya Prakash .jpg',
+        position:'Program Coordinator',
+        linkedIn:'https://www.linkedin.com/in/aishwarya--prakash/',
+        imgcorrect:false
+    },
+    {
+        id:11,
+        name:'Gautham Binu',
+        img:'/assets/execommember/GAUTHAM BINU.JPG',
+        position:'Web Team',
+        linkedIn:'https://www.linkedin.com/in/gautham-binu-60920524b/',
+        imgcorrect:true
+    },
+    {
+        id:12,
+        name:'Mahmood Abdul Razak',
+        img:'/assets/execommember/Mahmood Abdul Razak.jpg',
+        position:'Web Team',
+        linkedIn:'https://www.linkedin.com/in/mahmood-abdul-razak-a044a8321/',
+        imgcorrect:true
+    },
+    {
+        id:13,
+        name:'Sneha S K',
+        img:'/assets/execommember/S K Sneha.jpg',
+        position:'Design Team',
+        linkedIn:'https://www.linkedin.com/in/sneha-s-k-656117292/',
+        imgcorrect:true
+    },
+    {
+        id:14,
+        name:'Sruthin N',
+        img:'/assets/execommember/Sruthin N.JPG',
+        position:'Design Team',
+        linkedIn:'https://www.linkedin.com/in/sruthin-n-112615291/',
+        imgcorrect:true
+    },
+    {
+        id:14,
+        name:'Angelina Rose',
+        img:'/assets/execommember/Angelina Rose.JPG',
+        position:'Design Team',
+        linkedIn:'https://www.linkedin.com/in/angelina-rose-9b312b324/',
+        imgcorrect:true
+    },
+    {
+        id:13,
+        name:'GAUTHAM S',
+        img:'/assets/execommember/GAUTHAM S.jpg',
+        position:'Media Team',
+        linkedIn:'https://www.linkedin.com/in/gautham-s-a9873a256/',
+        imgcorrect:true
+    },
+    {
+        id:13,
+        name:'JASIM K',
+        img:'/assets/execommember/Jasim K Ismail.jpg',
+        position:'Media Team',
+        linkedIn:'https://www.linkedin.com/in/jasim-k-ismail-89b4a627a/',
+        imgcorrect:true
+    },
+    {
+        id:15,
+        name:'Sabarinath A',
+        img:'/assets/execommember/Sabarinath A.jpg',
+        position:'Documentation Team',
+        linkedIn:'https://www.linkedin.com/in/sabarinath-a-562180251/',
+        imgcorrect:false
+    },
+    {
+        id:16,
+        name:'Sreyas Warrier',
+        img:'/assets/execommember/Sreyas Warrier.jpg',
+        position:'Documentation Team',
+        linkedIn:"https://www.linkedin.com/in/sreyas-s-warrier-171a6a323/",
+        imgcorrect:true
+    },
+    {
+        id:17,
+        name:'Tanya Mariam ',
+        img:'/assets/execommember/Tanya Mariam .jpg',
+        position:'Documentation Team',
+        linkedIn:'https://www.linkedin.com/in/tanya-mariam-thomas-806196294/',
+        imgcorrect:true
+    },
+    {
+        id:18,
+        name:'Aditya RP',
+        img:'/assets/execommember/Aditya RP.jpg',
+        position:'Documentation Team',
+        linkedIn:'https://www.linkedin.com/in/aditya-rp-449599292/',
+        imgcorrect:true
+    },
+ 
 
 ]
 export const webteam24 =[
